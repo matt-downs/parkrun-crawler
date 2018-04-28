12832f77bf1a1ce2fe255295c433b49e231bc872
Matt Downs <mattdowns6@gmail.com>
2018-04-28 19:25:34 +1000

    refactor test for easier automated tests targetting different countries

--- __test__/getAthlete.test.js
+++ __test__/getAthlete.test.js
@@ -2,63 +2,70 @@ const Joi = require('Joi');
 const parkrunCrawler = require('../index');
 
 
-let athlete = {};
+[{
+    athleteId: '2054291',
+    country: 'au'
+}].forEach(testAthlete)
 
 
 
-describe('athlete', () => {
+function testAthlete(testData) {
+    return describe(`athlete from ${testData.country}`, () => {
 
-    beforeAll(async function () {
-        athlete = await parkrunCrawler.getAthlete('2054291', 'au');
-    });
+        let athlete = {};
 
-    test('name is a string', () => {
-        Joi.assert(athlete.name, Joi.string().required());
-    });
-
-    test('totalRuns is an integer', () => {
-        Joi.assert(athlete.totalRuns, Joi.number().integer().required());
-    });
-
-    test('recentRuns is an array containing the required data', () => {
-        let recentRunsSchema = Joi.object().keys({
-            event: Joi.string().required(),
-            eventUrl: Joi.string().required(),
-            date: Joi.string().required(),
-            genderPosition: Joi.string().required(),
-            overallPosition: Joi.string().required(),
-            time: Joi.string().required(),
-            ageGrade: Joi.string().required()
+        beforeAll(async function () {
+            athlete = await parkrunCrawler.getAthlete(testData.athleteId, testData.country);
         });
 
-        Joi.assert(athlete.recentRuns, Joi.array().items(recentRunsSchema).required());
-    });
-
-    test('eventSummary is an array containing the required data', () => {
-        let eventSummarySchema = Joi.object().keys({
-            event: Joi.string().required(),
-            eventUrl: Joi.string().required(),
-            runs: Joi.string().required(),
-            bestGenderPosition: Joi.string().required(),
-            bestOverallPosition: Joi.string().required(),
-            bestTime: Joi.string().required()
+        test('name is a string', () => {
+            Joi.assert(athlete.name, Joi.string().required());
         });
 
-        Joi.assert(athlete.eventSummary, Joi.array().items(eventSummarySchema).required());
+        test('totalRuns is an integer', () => {
+            Joi.assert(athlete.totalRuns, Joi.number().integer().required());
+        });
+
+        test('recentRuns is an array containing the required data', () => {
+            let recentRunsSchema = Joi.object().keys({
+                event: Joi.string().required(),
+                eventUrl: Joi.string().required(),
+                date: Joi.string().required(),
+                genderPosition: Joi.string().required(),
+                overallPosition: Joi.string().required(),
+                time: Joi.string().required(),
+                ageGrade: Joi.string().required()
+            });
+
+            Joi.assert(athlete.recentRuns, Joi.array().items(recentRunsSchema).required());
+        });
+
+        test('eventSummary is an array containing the required data', () => {
+            let eventSummarySchema = Joi.object().keys({
+                event: Joi.string().required(),
+                eventUrl: Joi.string().required(),
+                runs: Joi.string().required(),
+                bestGenderPosition: Joi.string().required(),
+                bestOverallPosition: Joi.string().required(),
+                bestTime: Joi.string().required()
+            });
+
+            Joi.assert(athlete.eventSummary, Joi.array().items(eventSummarySchema).required());
+        });
+
+        test('volunteerSummary is an array containing the required data', () => {
+            // let volunteerSummarySchema = Joi.object().keys({
+            //     event: Joi.string().required(),
+            //     eventUrl: Joi.string().required(),
+            //     date: Joi.string().required(),
+            //     genderPosition: Joi.string().required(),
+            //     overallPosition: Joi.string().required(),
+            //     time: Joi.string().required(),
+            //     ageGrade: Joi.string().required()
+            // });
+
+            // Joi.assert(athlete.volunteerSummary, Joi.array().items(volunteerSummarySchema).required());
+        });
+
     });
-
-    test('volunteerSummary is an array containing the required data', () => {
-        // let volunteerSummarySchema = Joi.object().keys({
-        //     event: Joi.string().required(),
-        //     eventUrl: Joi.string().required(),
-        //     date: Joi.string().required(),
-        //     genderPosition: Joi.string().required(),
-        //     overallPosition: Joi.string().required(),
-        //     time: Joi.string().required(),
-        //     ageGrade: Joi.string().required()
-        // });
-
-        // Joi.assert(athlete.volunteerSummary, Joi.array().items(volunteerSummarySchema).required());
-    });
-
-});
+}
